08e9725db1434237c768be7f75d7f8a823307d99
falikabiologi15 <falikabiologi15@gmail.com>
2026-05-17 22:11:57 +0700

    Update register.js

--- login/js/register.js
+++ login/js/register.js
@@ -16,9 +16,10 @@ document.getElementById("registerForm").addEventListener("submit", async functio
     const data = await res.json();
 
     if (data.status === "success") {
-        document.getElementById("message").innerText = "Registrasi berhasil, silakan login";
-        window.location.href = "index.html";
+        alert("Registrasi Berhasil! Silakan Klik OK Untuk Login.");
+        window.location.href = "index.html"; 
     } else {
         document.getElementById("message").innerText = data.message || "Gagal registrasi";
-    }
-});
+            }
+    });
+   
